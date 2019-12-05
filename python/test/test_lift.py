4dbd0585551641b4f3cba3ed06f43dfbc179a087
Emily Bache <emily.bache@praqma.com>
2019-12-05 11:15:21 +0100

    hide test that doesn't work yet

--- python/test/test_lift.py
+++ python/test/test_lift.py
@@ -4,8 +4,8 @@ from lift import Lift, LiftSystem
 from lift_printers import print_lifts
 
 
-# TODO: finish writing this test
-def test_something():
+# TODO: finish writing this test (remove the 'todo' prefix so the test will get run)
+def todo_test_something():
     liftA = Lift("A", 0)
     lifts = LiftSystem(floors=[0,1], lifts=[liftA])
     lifts.tick()
