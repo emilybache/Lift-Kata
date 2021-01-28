b9a7ca2d051aaa3e3645863da26e25c57033915d
Emily Bache <emily@bacheconsulting.com>
2021-01-28 08:18:20 +0000

    make the starting test more complete so it's clearer what to do

--- cpp/test/lift_catch.cpp
+++ cpp/test/lift_catch.cpp
@@ -2,12 +2,32 @@
 #include "catch2/catch.hpp"
 #include "ApprovalTests.hpp"
 
-TEST_CASE("Do Something") {
-    auto liftA = Lift("A", 0, false, std::set<int>());
+TEST_CASE("Answer a request") {
+    // ARRANGE
+    std::set<int> requests = std::set<int>();
+    requests.insert(1);
+    // create one lift that is initially on floor 0
+    // someone standing in the lift has requested floor 1
+    auto liftA = Lift("A", 0, true, requests);
     auto lift_system = LiftSystem(
             std::vector<Lift>{liftA},
             std::vector<int>{0, 1},
             std::vector<Call>());
+    std::stringstream toApprove;
+    toApprove << "INITIAL POSITION:\n";
+    toApprove << print_lifts(lift_system);
+
+    // ACT - need several ticks before request is fulfilled
     lift_system.tick();
-    ApprovalTests::Approvals::verify(print_lifts(lift_system));
+    toApprove << "\ntick()\n\n";
+    toApprove << print_lifts(lift_system);
+    lift_system.tick();
+    toApprove << "\ntick()\n\n";
+    toApprove << print_lifts(lift_system);
+    lift_system.tick();
+    toApprove << "\ntick()\n\n";
+    toApprove << print_lifts(lift_system);
+
+    // ASSERT
+    ApprovalTests::Approvals::verify(toApprove.str());
 }
